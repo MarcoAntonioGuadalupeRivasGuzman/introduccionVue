1fb77f772b01695c38bfebc1f0cc008cc52d5adc
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-02-17 16:56:32 -0600

    se agrego un objeto que tiene frases para practicar el v-for

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 
+//data para trabajar
+const quotes = [
+    { quote: 'The night is darkest just before the dawn. And I promise you, the dawn is coming.', author: 'Harvey Dent, The Dark Knight' },
+    { quote: 'I believe what doesn’t kill you simply makes you, stranger.', author: 'The Joker, The Dark Knight' },
+    { quote: 'Your anger gives you great power. But if you let it, it will destroy you… As it almost did me', author: 'Henri Ducard, Batman Begins' },
+    { quote: 'You either die a hero or live long enough to see yourself become the villain.', author: 'Harvey Dent, The Dark Knight' },
+    { quote: 'If you’re good at something, never do it for free.', author: 'The Joker, The Dark Knight' },
+    { quote: 'Yes, father. I shall become a bat.', author: 'Bruce Wayne/Batman, Batman: Year One' },
+]
+
+
 const app = Vue.createApp({
     /*template:`
     <h1>Hola mundo</h1>
@@ -8,10 +19,11 @@ const app = Vue.createApp({
     data(){ //se esta trabajando el estado del componente
         return {
             message: 'Hola a todos',
-            manager: 'Mr robot'
+            manager: 'Mr robot',
+            citas: quotes
         }
     },
-    methods:{
+    methods:{ //se declaran los metodos que tendra el objeto
         changeMessage(event){
             console.log('hi',event);
             this.message="hi stalker";
