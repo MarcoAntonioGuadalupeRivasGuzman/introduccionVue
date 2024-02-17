09b30c471f27e2df35ec3f9b666facbcb737cef0
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-02-17 16:29:44 -0600

    se agregaron metodos que se vincularon a un evento

--- app.js
+++ app.js
@@ -7,7 +7,18 @@ const app = Vue.createApp({
 
     data(){ //se esta trabajando el estado del componente
         return {
-            message: 'Hola a todos'
+            message: 'Hola a todos',
+            manager: 'Mr robot'
+        }
+    },
+    methods:{
+        changeMessage(event){
+            console.log('hi',event);
+            this.message="hi stalker";
+            this.changeManager();
+        },
+        changeManager(){
+            this.manager="Steve chambitas";
         }
     }
 });
